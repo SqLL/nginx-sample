4cb1cf60648d7f0309de348cce397c549fd31c1e
sqll <sqll@protonmail.com>
2018-08-25 17:41:57 +0200

    Change settings for building instead of nginx

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
-ng build --prod;
+# --output-hashing all — hash contents of the generated files and append hash
+# to the file name to facilitate browser cache busting (any change to file
+# content will result in different hash and hence browser is forced to load
+# a new version of the file)
+ng build --prod --output-hashing=all
 docker build -t custom-nginx .
